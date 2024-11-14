0de78276f6a3371bf0704c7dcb94e2ede56d2481
hrofft <hedinhroft@gmail.com>
2024-11-14 10:01:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#1st program
+print((9**0.5)*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+#4th program
+print('1234.56')
+print(type(float('123.456')))
+print(123.456*10)
+print(type(int(1234.56)))
+print(1234%10)
+
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
