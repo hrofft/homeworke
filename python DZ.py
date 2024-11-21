9e8df0c92a564a485fa59f79187beca799cb3680
hrofft <hedinhroft@gmail.com>
2024-11-21 08:32:07 +0300

    Add files via upload

--- python DZ.py
+++ python DZ.py
@@ -1,7 +1,8 @@
-name="Alexey:"
-Number_of_completed_DPS="total issues:12"
-Number_of_hours_spent="hours spent:1.5"
-Course_name="Course:Python"
-Time_per_task=1.5/12
-average_lead_time="average_lead_time:"
-print(name,Course_name,Number_of_completed_DPS,Number_of_hours_spent,average_lead_time,Time_per_task,)
+from tkinter.scrolledtext import example
+
+example='Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
