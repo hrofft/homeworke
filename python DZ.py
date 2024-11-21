e54460d89540aa3f06698c1c2d543a4cbcdb6846
hrofft <hedinhroft@gmail.com>
2024-11-21 07:35:46 +0300

    Add files via upload

---
+++ python DZ.py
@@ -0,0 +1,7 @@
+name="Alexey:"
+Number_of_completed_DPS="total issues:12"
+Number_of_hours_spent="hours spent:1.5"
+Course_name="Course:Python"
+Time_per_task=1.5/12
+average_lead_time="average_lead_time:"
+print(name,Course_name,Number_of_completed_DPS,Number_of_hours_spent,average_lead_time,Time_per_task,)
