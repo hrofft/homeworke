d319ac9a8ef77658cdc1bd1eb2854bb9106de7a5
hrofft <hedinhroft@gmail.com>
2024-11-22 08:16:02 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,7 @@
+my_string=input("Практика по теме :")
+print("В названии темы:",len(my_string),"символов")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
